839689e90a49b710d404b40820b39ed7498ceacf
andreas <xandreasjuenx@gmail.com>
2022-11-24 21:15:45 +0100

    inital

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "MaterialRipple",
+    platforms: [
+       .iOS(.v9),
+    ],
+    products: [
+        .library(
+            name: "MaterialRipple",
+            targets: ["MaterialRipple"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "MaterialRipple",
+            dependencies: [],
+            path: "Sources"),
+    ]
+)
